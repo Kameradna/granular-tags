f9513fd71128bcadeb4aec135d9e7ac139b1bc3a
Kameradna <pjicasper@gmail.com>
2022-05-09 18:18:41 +1000

    Added addition print lines to reassure of progress

--- unzipIUXray.py
+++ unzipIUXray.py
@@ -1,6 +1,7 @@
 import zipfile
 
 with zipfile.ZipFile("chest-xrays-indiana-university.zip","r") as zip_ref:
+    print("Extracting chest-xrays-indiana-university.")
     zip_ref.extractall("IU_xray_data")
 
 print('Done.')
