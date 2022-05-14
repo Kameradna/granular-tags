39cece69a3c2e54e727244f497f18df2a133d0dc
Kameradna <pjicasper@gmail.com>
2022-05-14 22:08:04 +1000

    renamed xmlify

--- txtify.py
+++ txtify.py
@@ -13,12 +13,12 @@ file_df = pd.read_csv(f'{iu_folder_path}/{input_name}')
 
 for row in file_df.itertuples():
     with open(f'{txt_dir}/{row.uid}.txt','w') as f:
-        f.write(f'uid: {row.uid}\n') #commenting out lines here then rerunning this and the text2bioc allows us to eliminate some of this from the pipeline
-        f.write(f'mesh: {row.MeSH}\n')
-        f.write(f'problems: {row.Problems}\n')
-        f.write(f'image: {row.image}\n')
-        f.write(f'indication: {row.indication}\n')
-        f.write(f'comparison: {row.comparison}\n')
+        # f.write(f'uid: {row.uid}\n') #commenting out lines here then rerunning this and the text2bioc allows us to eliminate some of this from the pipeline
+        # f.write(f'mesh: {row.MeSH}\n')
+        # f.write(f'problems: {row.Problems}\n')
+        # f.write(f'image: {row.image}\n')
+        # f.write(f'indication: {row.indication}\n')
+        # f.write(f'comparison: {row.comparison}\n')
         f.write(f'findings: {row.findings}\n')
         f.write(f'impression: {row.impression}\n')
 print('success')
