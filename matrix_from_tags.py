f911e5fc3da3aee2382550c0ae530f56a045871f
Kameradna <pjicasper@gmail.com>
2022-05-15 15:27:59 +1000

    matrix_from_tags gives you numbers of tags in latex tabular ready format

--- matrix_from_tags.py
+++ matrix_from_tags.py
@@ -82,7 +82,7 @@ def matrix_from_tags(descriptor_dict):
             print(f'Removing {descriptor} from tag list')
     print(f'{len(unique_tags_list)} unique tags after truncating...')
 
-    for descriptor in unique_count:
+    for descriptor in sorted(unique_count):
         print(f"{descriptor}& {unique_count[descriptor]}\\\\")
     
     with open(f'{args.save_dir}/unique_tags_list.json','w') as f:
