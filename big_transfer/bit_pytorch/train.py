c9dec7adcd20028fa381d80fd27d955095a8a357
Kameradna <pjicasper@gmail.com>
2022-05-15 20:03:29 +1000

    removed logitloss, do not use am

--- big_transfer/bit_pytorch/train.py
+++ big_transfer/bit_pytorch/train.py
@@ -472,7 +472,7 @@ if __name__ == "__main__":
   parser.add_argument("--workers", type=int, default=8,
                       help="Number of background threads used to load data.")
   parser.add_argument("--no-save", dest="save", action="store_false")
-  parser.add_argument("--use_amp", required=True, dest="use_amp",action="store_true",
+  parser.add_argument("--use_amp", dest="use_amp",action="store_true",
                      help="Use Automated Mixed Precision to save potential memory and compute?")
   parser.add_argument("--annodir", required=True, help="Where are the annotation files to load?")
   parser.add_argument("--chexpert", dest="chexpert", action="store_true",help="Run as the chexpert paper?")
