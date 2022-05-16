c8c4e11e7f7bf64396b4662d1d0e97c464292068
Kameradna <pjicasper@gmail.com>
2022-05-16 22:28:37 +1000

    AUC parallel, added .data,,,,

--- big_transfer/bit_pytorch/train.py
+++ big_transfer/bit_pytorch/train.py
@@ -287,6 +287,8 @@ def run_eval(model, data_loader, device, chrono, logger, args, step, dataset):
         groundtruth = torch.ge(y,0.5)#translates y to tensor
         y_true = groundtruth.cpu().numpy() if isinstance(y_true, type(None)) else np.concatenate((y_true,groundtruth.cpu().numpy()))
         y_logits = logits.cpu().numpy() if isinstance(y_logits, type(None)) else np.concatenate((y_logits,logits.cpu().numpy()))
+        print(loss)
+        print(c_num)
         loss = c_num if isinstance(loss, type(None)) else np.concatenate((loss,c_num))
 
     # measure elapsed time
