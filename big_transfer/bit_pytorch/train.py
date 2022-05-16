680aeddc0adc58ca84286c5ab0d43fc04c3bbd2e
Kameradna <pjicasper@gmail.com>
2022-05-16 22:38:10 +1000

    New validation, no more specialised AUC, all numpy operations and sklean based

--- big_transfer/bit_pytorch/train.py
+++ big_transfer/bit_pytorch/train.py
@@ -297,8 +297,8 @@ def run_eval(model, data_loader, device, chrono, logger, args, step, dataset):
   auroc = metrics.roc_auc_score(y_true,y_logits,average=None,labels=dataset.classes)#should we pass in labels?
   y_pred = y_logits > 0.5
 
-  print(y_true)
-  print(y_pred)
+  print(np.size(y_true))
+  print(np.size(y_pred))
 
   accuracy = metrics.accuracy_score(y_true,y_pred)#I think this is exact matches
   precision, recall, f1, support = metrics.precision_recall_fscore_support(y_true,y_pred,labels=dataset.classes,average='macro')   #this will raise warnings, if you want to turn off, add zero_division=0 or 1
